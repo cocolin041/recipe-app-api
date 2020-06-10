8504cfd278bb46705fa2271b30f29a82696bb453
SihYi Lin <sihyiyl2@illinois.edu>
2020-06-10 16:13:58 +0800

    Configured postgres db.

--- app/core/models.py
+++ app/core/models.py
@@ -8,7 +8,7 @@ class UserManager(BaseUserManager):
     """Creates and saves a new user"""
     if not email:
       raise ValueError('Users must have an email address.')
-    user = self.model(email=self.normalize_email(email), **extra_fields)
+    user = self.model(email=self.normalize_email(email), **extra_fields) 
     user.set_password(password)
     user.save(using=self._db)
 
@@ -26,7 +26,8 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
   """Custom user model that supports using email instead of username"""
   email = models.EmailField(max_length=255, unique=True)
-  name = models.CharField(max_length=255),
+  name = models.CharField(max_length=255)
+  is_active = models.BooleanField(default=True)
   is_staff = models.BooleanField(default=False)
 
   objects = UserManager()
